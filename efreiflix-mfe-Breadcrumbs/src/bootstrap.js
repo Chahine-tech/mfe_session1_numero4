507a17a619bc947d62ac9b02f652fa6b6d2cf2db
Yousseflamrani <yousseflamrani2001@gmail.com>
2025-02-11 17:22:00 +0100

    add Fil d'Ariane

--- efreiflix-mfe-Breadcrumbs/src/bootstrap.js
+++ efreiflix-mfe-Breadcrumbs/src/bootstrap.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Skeleton from './Skeleton';
+import Skeleton from './Breadcrumbs';
 
 const mount = (el) => {
   const root = createRoot(el);
