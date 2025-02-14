6445171fdb0ce37819fabc496d1e31b8ce7e64a8
Yousseflamrani <yousseflamrani2001@gmail.com>
2025-02-14 09:24:06 +0100

    Ajouter du style et ajout de l'icon

--- efreiflix-mfe-Breadcrumbs/src/Breadcrumbs.js
+++ efreiflix-mfe-Breadcrumbs/src/Breadcrumbs.js
@@ -2,28 +2,27 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 function handleClick(event) {
     event.preventDefault();
     console.info('You clicked a breadcrumb.');
 }
 
-export default function BasicBreadcrumbs() {
+export default function CustomBreadcrumbs() {
     return (
-        <div role="presentation" onClick={handleClick}>
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link underline="hover" color="inherit" href="/">
-                    MUI
-                </Link>
-                <Link
-                    underline="hover"
-                    color="inherit"
-                    href="/material-ui/getting-started/installation/"
-                >
-                    Core
-                </Link>
-                <Typography sx={{ color: 'text.primary' }}>Breadcrumbs</Typography>
-            </Breadcrumbs>
-        </div>
+        <Breadcrumbs
+            separator={<NavigateNextIcon fontSize="small" />}
+            aria-label="breadcrumb"
+        >
+            <Link underline="hover" color="inherit" href="/">
+                <HomeIcon fontSize="small" sx={{ verticalAlign: 'middle', mb: 0.2 }} />
+            </Link>
+            <Link underline="hover" color="inherit" href="/projects">
+                Projects
+            </Link>
+            <Typography color="text.primary">Project Nero</Typography>
+        </Breadcrumbs>
     );
 }
